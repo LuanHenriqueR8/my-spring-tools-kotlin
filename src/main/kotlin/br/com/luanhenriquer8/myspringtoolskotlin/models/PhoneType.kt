a4450d6cc5e016be4b5acdecdb8a0e9d0d7ea467
Luan Santos <luanhenriquer8@gmail.com>
2020-03-03 22:41:01 -0300

    mapping id of models

--- src/main/kotlin/br/com/luanhenriquer8/myspringtoolskotlin/models/PhoneType.kt
+++ src/main/kotlin/br/com/luanhenriquer8/myspringtoolskotlin/models/PhoneType.kt
@@ -1,4 +1,13 @@
 package br.com.luanhenriquer8.myspringtoolskotlin.models
 
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
 class PhoneType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long = 0
 }
