5b2f72448159207e90e6c0f3379190ae98319b14
Luan Santos <luanhenriquer8@gmail.com>
2020-03-03 22:35:28 -0300

    add project

---
+++ src/main/kotlin/br/com/luanhenriquer8/myspringtoolskotlin/MySpringToolsKotlinApplication.kt
@@ -0,0 +1,11 @@
+package br.com.luanhenriquer8.myspringtoolskotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MySpringToolsKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<MySpringToolsKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/luanhenriquer8/myspringtoolskotlin/MySpringToolsKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.luanhenriquer8.myspringtoolskotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MySpringToolsKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
