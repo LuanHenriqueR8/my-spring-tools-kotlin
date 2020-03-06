b0f4d783b2e238479162770247760c907c21d888
Luan Santos <luanhenriquer8@gmail.com>
2020-03-05 23:11:51 -0300

    change service

---
+++ src/main/kotlin/br/com/luanhenriquer8/myspringtoolskotlin/services/PersonService.kt
@@ -0,0 +1,28 @@
+package br.com.luanhenriquer8.myspringtoolskotlin.services
+
+import br.com.luanhenriquer8.myspringtoolskotlin.models.Person
+import br.com.luanhenriquer8.myspringtoolskotlin.repositories.PersonRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+@Service
+class PersonService {
+
+    @Autowired
+    private lateinit var personRepository: PersonRepository
+
+    fun findOnePerson(id: Long) = personRepository.findById(id)
+
+    fun findAllPersons() = personRepository.findAll()
+
+//    fun createPerson() = personRepository.save()
+
+    fun updatePerson() {
+
+    }
+
+    fun createPerson(person: Person) = personRepository.save(person)
+
+
+//    fun deletePerson() = personRepository.delete()
+}
